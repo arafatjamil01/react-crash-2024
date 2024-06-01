6d2a17c0d3d9cfae6cc7af719244237198cddeaf
Arafat Jamil <arafatjamil01@gmail.com>
2024-06-01 14:24:47 +0600

    Add job, and update job page created, forms

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,57 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
+  // Add job
+  const addJob = async (newjob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newjob),
+    });
+
+    return;
+  }
+
+  // Delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+
+    return;
+  } 
+
+  // Update job
+  const updateJob = async (updatedJob) => {
+    const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedJob),
+    });
+
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/edit-jobs/:id" element={<EditJobPage updatJobSubmit={updateJob} />} loader={jobLoader} />
+        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,23 @@
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useParams, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import {toast} from 'react-toastify';
 
-const JobPage = () => {
-//   const { id } = useParams();
+const JobPage = ({ deleteJob }) => {
+  const { id } = useParams();
   const job = useLoaderData();
+  const navigate = useNavigate();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm('Are you sure you want to delete this job?');
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully');
+    navigate('/jobs');
+  };
 
   return (
     <>
@@ -14,8 +27,8 @@ const JobPage = () => {
             to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
-			<FaArrowLeft className="mr-2" />
-             Back to Job Listings
+            <FaArrowLeft className="mr-2" />
+            Back to Job Listings
           </Link>
         </div>
       </section>
@@ -26,12 +39,10 @@ const JobPage = () => {
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
                 <div className="text-gray-500 mb-4">{job.type}</div>
-                <h1 className="text-3xl font-bold mb-4">
-                  {job.title}
-                </h1>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
                   <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-				  <FaMapMarker className='fa-solid text-lg text-orange-700 mr-2' />
+                  <FaMapMarker className="fa-solid text-lg text-orange-700 mr-2" />
                   <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
@@ -41,9 +52,7 @@ const JobPage = () => {
                   Job Description
                 </h3>
 
-                <p className="mb-4">
-                  {job.description}
-                </p>
+                <p className="mb-4">{job.description}</p>
 
                 <h3 className="text-indigo-800 text-lg font-bold mb-2">
                   {job.salary}
@@ -61,9 +70,7 @@ const JobPage = () => {
 
                 <h2 className="text-2xl">{job.company.name}</h2>
 
-                <p className="my-2">
-                  {job.company.description}
-                </p>
+                <p className="my-2">{job.company.description}</p>
 
                 <hr className="my-4" />
 
@@ -75,19 +82,24 @@ const JobPage = () => {
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
               </div>
 
               {/* <!-- Manage --> */}
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/edit/jobs/${job.id}`}
+                  to={`/edit-jobs/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
